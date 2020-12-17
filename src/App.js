2583f8f00f61877dc6c77a800c0a453f458978b3
Burak Bilgin <burakbilgin07@gmail.com>
2020-12-17 23:30:57 +0300

    api entegration

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css'
 import Card from './components/Card'
 import Nav from './components/Nav'
 import Button from './components/Button'
+import axios from "axios";
 
 class App extends React.Component{
+
+  state = {
+    movies:[]
+  }
+  componentDidMount(){
+    axios.get('http://localhost:3001/movies')
+    .then(res => this.setState({movies: res.data}))
+  }
  
   render(){
   return (
